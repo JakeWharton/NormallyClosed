b70b7c685557a80fd8b8b1cf71db03f90ce06555
Jake Wharton <jw@squareup.com>
2021-04-18 23:06:52 -0400

    Support single and multi-button control
    This moves config from the CLI to a TOML file.

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,100 @@
-use structopt::clap::ArgGroup;
-use structopt::clap::Error;
+use std::error::Error;
+use std::fs;
+
+use structopt::clap::Error as ClapError;
 use structopt::clap::ErrorKind::InvalidValue;
-use structopt::StructOpt;
 use tracing::debug;
 
-use crate::pi::Garage;
+use crate::config::GarageConfig;
+use crate::config::RelayConfig;
+use crate::garage::Garage;
+
+mod cli;
+
+mod config;
+
+mod garage;
 
 mod http;
+
+#[cfg(feature = "rpi")]
 mod pi;
 
-#[derive(Debug, Clone)]
-pub struct Door {
-	/// User-friendly name
-	pub name: String,
-	/// BCP GPIO pin number
-	pub pin: u8,
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn Error>> {
 	tracing_subscriber::fmt::init();
 
-	let args: Args = Args::from_args();
+	let args = cli::parse_args();
 	debug!("{:?}", &args);
 
-	let relays = if let Some(board) = args.board {
-		if !args.gpio.is_empty() {
-			Error::with_description("Specify one of --board or --gpio, not both", InvalidValue).exit();
-		}
-		match board.as_ref() {
-			// https://pinout.xyz/pinout/automation_hat_mini
-			"PIM487" => vec![16u8],
-			// https://pinout.xyz/pinout/automation_phat
-			"PIM221" => vec![16u8],
-			// https://pinout.xyz/pinout/automation_hat
-			"PIM213" => vec![13u8, 19u8, 16u8],
-			// https://bc-robotics.com/shop/raspberry-pi-zero-relay-hat/
-			// https://bc-robotics.com/shop/raspberry-pi-zero-relay-hat-assembled/
-			"RAS-109" | "RAS-194" => vec![4u8, 17u8],
-			_ => {
-				Error::with_description("Unknown board model", InvalidValue).exit();
-			}
-		}
-	} else {
-		if args.gpio.is_empty() {
-			Error::with_description("One of --board or --gpio must be specified", InvalidValue).exit();
-		}
-		args.gpio
-	};
-	debug!("Relays pins {:?}", &relays);
+	let config = fs::read_to_string(args.config_file)?;
+	let config = config::parse_config(&config)?;
+	debug!("{:?}", &config);
 
-	if args.doors < 1 || args.doors > relays.len() {
-		Error::with_description(
-			&format!(
-				"Door count ({}) must not exceed available board relay count ({})",
-				args.doors,
-				relays.len()
-			),
-			InvalidValue,
-		)
-		.exit();
-	}
-
-	if args.names.len() > args.doors {
-		Error::with_description(
-			&format!(
-				"Name count ({}) must not exceed door count ({})",
-				args.names.len(),
-				args.doors
-			),
-			InvalidValue,
-		)
-		.exit();
-	}
-
-	let mut doors = vec![];
-	for i in 0..args.doors {
-		doors.push(Door {
-			name: args
-				.names
-				.get(i)
-				.map(|name| name.to_string())
-				.unwrap_or_else(|| format!("Door {}", i + 1)),
-			pin: relays[i],
-		});
-	}
-
-	let garage = Garage::new(doors)?;
+	let garage = create_garage(&config)?;
 
 	http::listen(garage, args.http_port).await;
-
 	Ok(())
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt(group = ArgGroup::with_name("pins").required(true))]
-struct Args {
-	/// Manufactured HAT board with preconfigured relays
-	///
-	/// PIM213 - Pimoroni automation HAT mini with 1 relay
-	///{n}PIM221 - Pimoroni automation pHAT with 1 relay
-	///{n}PIM487 - Pimoroni automation HAT with 3 relays
-	///{n}RAS-109 - BC Robotics relay HAT with 2 relays.
-	///{n}RAS-194 - BC Robotics relay HAT (assembled) with 2 relays.
-	///{n}
-	#[structopt(long, name = "model", group = "pins")]
-	board: Option<String>,
+fn create_garage(config: &GarageConfig) -> Result<Garage, Box<dyn Error>> {
+	let pins = match &config.relays {
+		RelayConfig::BoardBased { board } => {
+			match board.as_ref() {
+				// https://pinout.xyz/pinout/automation_hat_mini
+				"PIM487" => vec![16u8],
+				// https://pinout.xyz/pinout/automation_phat
+				"PIM221" => vec![16u8],
+				// https://pinout.xyz/pinout/automation_hat
+				"PIM213" => vec![13u8, 19u8, 16u8],
+				// https://bc-robotics.com/shop/raspberry-pi-zero-relay-hat/
+				// https://bc-robotics.com/shop/raspberry-pi-zero-relay-hat-assembled/
+				"RAS-109" | "RAS-194" => vec![4u8, 17u8],
+				_ => {
+					ClapError::with_description("Unknown board model", InvalidValue).exit();
+				}
+			}
+		}
+		RelayConfig::PinBased { pins } => pins.to_vec(),
+	};
+	debug!("Relays pins {:?}", &pins);
 
-	/// Custom BCP GPIO pin number to trigger a door relay
-	#[structopt(long, name = "pin", group = "pins")]
-	gpio: Vec<u8>,
+	if config.doors.is_empty() {
+		ClapError::with_description("No doors defined", InvalidValue).exit();
+	}
 
-	/// Number of doors
-	#[structopt(long, name = "count")]
-	doors: usize,
+	let door_relay_count = config
+		.doors
+		.iter()
+		.fold(0, |count, door| count + door.relay_count()) as usize;
+	if door_relay_count > pins.len() {
+		ClapError::with_description(
+			&format!(
+				"Door relay usage ({}) must not exceed available board relay count ({})",
+				door_relay_count,
+				pins.len(),
+			),
+			InvalidValue,
+		)
+		.exit();
+	}
 
-	/// User-friendly names for the doors
-	#[structopt(name = "NAME")]
-	names: Vec<String>,
+	create_garage_implementation(config, &pins)
+}
 
-	/// HTTP port
-	#[structopt(long, name = "port", default_value = "31415")]
-	http_port: u16,
+#[cfg(feature = "rpi")]
+fn create_garage_implementation(
+	config: &GarageConfig,
+	pins: &Vec<u8>,
+) -> Result<Garage, Box<dyn Error>> {
+	pi::create_garage(config, pins)
+}
+
+#[cfg(not(feature = "rpi"))]
+fn create_garage_implementation(
+	_config: &GarageConfig,
+	_pins: &Vec<u8>,
+) -> Result<Garage, Box<dyn Error>> {
+	eprintln!("Cannot start without 'rpi' feature");
+	std::process::exit(1);
 }
